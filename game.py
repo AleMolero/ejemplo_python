7aef936caade868f17ea46a39cc2617b817d97c4
AleMolero <Ale>
2023-03-16 00:16:15 -0300

    -> sin división por 0

--- game.py
+++ game.py
@@ -26,6 +26,8 @@ for i in range(0, times):
         case "*":
             correct= number_1 * number_2
         case "/":
+            while (number_2==0):
+                number_2 = randrange(10)
             correct= number_1 / number_2
     correct= float(correct)
     correct= round(correct,2) 
